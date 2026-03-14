be76c5804ec688eb622eeac2f5ba9e334ac4fe9e
Frodo1997 <ecruzmunoz7@gmail.com>
2026-03-14 07:02:18 -0600

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,14 @@
+export const translations = {
+  es: {
+    title: "Tienda Online",
+    add: "Agregar al carrito"
+  },
+  en: {
+    title: "Online Store",
+    add: "Add to cart"
+  },
+  pt: {
+    title: "Loja Online",
+    add: "Adicionar ao carrinho"
+  }
+};
